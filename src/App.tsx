f11dd985f0a7bfdb21eeba81ec2f7b6aa44adb02
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-04-10 19:57:42 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Box, createTheme, ThemeProvider } from '@mui/material';
 import { Header } from './components/Header';
+import { Layout } from './components/Layout';
 
 
 const theme = createTheme({})
@@ -12,12 +13,12 @@ function App() {
     <ThemeProvider theme={theme}>
       <Box
         component="main"
-        sx={{
-          height: '100vh',
-          bgcolor: '#000'
-        }}
+        sx={{height: '100vh'}}
       >
         <Header />
+        <Layout>
+          <h1>Olá mundo</h1>
+        </Layout>
       </Box>
     </ThemeProvider>
   );
---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function Layout({children}: Props) {
+  return (
+    <Box>
+      <Container
+        maxWidth="lg"
+        sx={{mt: 4, mb: 4,  backgroundColor: '#000'}}
+      >
+        {children}
+      </Container>
+    </Box>
+  );
+}
