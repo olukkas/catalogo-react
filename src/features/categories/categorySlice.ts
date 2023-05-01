1a094d92b689d740e13a3093c70284581c498091
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-01 14:43:35 -0300

    feat(categories): git integration commands

--- src/features/categories/categorySlice.ts
+++ src/features/categories/categorySlice.ts
@@ -1,1 +1,12 @@
+const category = {
+    id: '0',
+    name: 'Olive',
+    description: 'Earum quote',
+    is_active: true,
+    deleted_at: null,
+    created_at: '2023-08-15T10:59:09+0000',
+    updated_at: '2023-08-15T10:59:09+0000',
+}
+
+
 export const initialState = {}
