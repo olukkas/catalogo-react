0579359318623770b0a466e1191d2728aed596cb
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-06 13:28:15 -0300

    feat(categories): render table example

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,19 +1,40 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectcategories } from "./categorySlice";
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
 
 export const ListCategory = () => {
 
     const categories = useAppSelector(selectcategories)
 
+    const rows: GridRowsProp = [
+        { id: 1, col1: 'Hello', col2: 'World' },
+        { id: 2, col1: 'DatagridPro', col2: 'isAwsome' },
+        { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    ]
+
+    const columns: GridColDef[] = [
+        { field: 'col1', headerName: 'Column 1', width: 150 },
+        { field: 'col2', headerName: 'Column 2', width: 150 },
+    ];
+
+
     return (
-        <Box>
-            <Typography variant={'h3'} component={'h1'}>
-                ListCategory
-                {categories.map(category => (
-                    <Typography key={category.id}>{category.name}</Typography>
-                ))}
-            </Typography>
+        <Box maxWidth={'lg'} sx={{ mt: 4, mb: 4 }}>
+            <Box display={'flex'} justifyContent="flex-end">
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    component={Link}
+                    href='/categories/create'
+                    style={{ marginBottom: '1rem' }}
+                >
+                    New Category
+                </Button>
+            </Box>
+            <div style={{ height: 300, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} />
+            </div>
         </Box>
     );
 }
