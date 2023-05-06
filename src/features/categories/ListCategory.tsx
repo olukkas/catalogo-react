6c84a6bf9528748ddd8dffca502da71649cb4f96
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-06 13:41:01 -0300

    feat(categories): renderizando dados de categorias na tabela

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Link, Typography } from "@mui/material";
+import { Box, Button, Link, } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectcategories } from "./categorySlice";
 import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
@@ -7,15 +7,16 @@ export const ListCategory = () => {
 
     const categories = useAppSelector(selectcategories)
 
-    const rows: GridRowsProp = [
-        { id: 1, col1: 'Hello', col2: 'World' },
-        { id: 2, col1: 'DatagridPro', col2: 'isAwsome' },
-        { id: 3, col1: 'MUI', col2: 'is Amazing' },
-    ]
+    const rows: GridRowsProp = categories.map(category => ({
+        id: category.id,
+        name: category.name,
+        description: category.description
+    }))
 
     const columns: GridColDef[] = [
-        { field: 'col1', headerName: 'Column 1', width: 150 },
-        { field: 'col2', headerName: 'Column 2', width: 150 },
+        { field: 'id', headerName: 'ID', width: 150 },
+        { field: 'name', headerName: 'Name', width: 150 },
+        { field: 'description', headerName: 'Description', width: 150 },
     ];
 
 
