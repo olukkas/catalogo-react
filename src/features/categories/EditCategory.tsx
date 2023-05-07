e96002a4990b8dbd76f14584a66a8aae63bcf374
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-06 22:02:03 -0300

    style: formatação

--- src/features/categories/EditCategory.tsx
+++ src/features/categories/EditCategory.tsx
@@ -1,11 +1,11 @@
 import { Box, Typography } from "@mui/material";
 
 export const EditCategory = () => {
-  return (
-    <Box>
-      <Typography variant={'h3'} component={'h1'}>
-        EditCategory
-      </Typography>
-    </Box>
-  );
+    return (
+        <Box>
+            <Typography variant={'h3'} component={'h1'}>
+                EditCategory
+            </Typography>
+        </Box>
+    );
 }
