645811e23235890a41a4e80bc70ba22e74e8d3ae
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-06 23:19:35 -0300

    style: chance indentation for tsx files for 2 spaces

--- src/features/categories/EditCategory.tsx
+++ src/features/categories/EditCategory.tsx
@@ -1,41 +1,41 @@
 import {
-    Box,
-    FormControl,
-    Grid,
-    Paper,
-    TextField,
-    Typography
+  Box,
+  FormControl,
+  Grid,
+  Paper,
+  TextField,
+  Typography
 } from "@mui/material";
 
 export const EditCategory = () => {
-    return (
-        <Box>
-            <Paper>
-                <Box p={2}>
-                    <Box mb={2}>
-                        <Typography variant="h4">Edit Category</Typography>
-                    </Box>
-                </Box>
-
-                <Box p={2}>
-                    <form>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <FormControl fullWidth>
-                                    <TextField
-                                        required
-                                        name='name'
-                                        label='name'
-                                        value={'0'}
-                                        disabled={false}
-                                        onChange={console.log}
-                                    />
-                                </FormControl>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </Box>
-            </Paper>
+  return (
+    <Box>
+      <Paper>
+        <Box p={2}>
+          <Box mb={2}>
+            <Typography variant="h4">Edit Category</Typography>
+          </Box>
         </Box>
-    );
+
+        <Box p={2}>
+          <form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <TextField
+                    required
+                    name='name'
+                    label='name'
+                    value={'0'}
+                    disabled={false}
+                    onChange={console.log}
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -3,115 +3,115 @@ import { useAppSelector } from "../../app/hooks";
 import { selectcategories } from "./categorySlice";
 import DeleteIcon from '@mui/icons-material/Delete'
 import {
-    DataGrid,
-    GridColDef,
-    GridRenderCellParams,
-    GridRowsProp,
-    GridToolbar
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridRowsProp,
+  GridToolbar
 } from '@mui/x-data-grid'
 
 export const ListCategory = () => {
 
-    const categories = useAppSelector(selectcategories)
+  const categories = useAppSelector(selectcategories)
 
-    const componetsdProps = {
-        toolbar: {
-            showQuickFilter: true,
-            quickFilterProps: { debounceMs: 500 }
-        }
+  const componetsdProps = {
+    toolbar: {
+      showQuickFilter: true,
+      quickFilterProps: { debounceMs: 500 }
     }
+  }
 
-    const rows: GridRowsProp = categories.map(category => ({
-        id: category.id,
-        name: category.name,
-        isActive: category.is_active,
-        createdAt: new Date(category.created_at).toLocaleDateString('pt-BR')
-    }))
+  const rows: GridRowsProp = categories.map(category => ({
+    id: category.id,
+    name: category.name,
+    isActive: category.is_active,
+    createdAt: new Date(category.created_at).toLocaleDateString('pt-BR')
+  }))
 
-    const columns: GridColDef[] = [
-        {
-            field: 'name',
-            headerName: 'Name',
-            flex: 1,
-            renderCell: renderNameCell
-        },
-        {
-            field: 'isActive',
-            headerName: 'Active',
-            flex: 1,
-            type: 'boolean',
-            renderCell: renderIsActiveCell,
-        },
-        {
-            field: 'createdAt',
-            headerName: 'Create at',
-            flex: 1,
-        },
-        {
-            field: 'id',
-            headerName: 'Actions',
-            flex: 1,
-            renderCell: renderActionsCell
-        },
-    ];
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      renderCell: renderNameCell
+    },
+    {
+      field: 'isActive',
+      headerName: 'Active',
+      flex: 1,
+      type: 'boolean',
+      renderCell: renderIsActiveCell,
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Create at',
+      flex: 1,
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      flex: 1,
+      renderCell: renderActionsCell
+    },
+  ];
 
-    function renderIsActiveCell(row: GridRenderCellParams) {
-        return (
-            <Typography>
-                {row.value ? 'Active' : 'Inactive'}
-            </Typography>
-        )
-    }
-
-    function renderActionsCell(row: GridRenderCellParams) {
-        return (
-            <IconButton
-                color='secondary'
-                onClick={() => console.log(row.value)}
-                aria-label='delete'
-            >
-                <DeleteIcon />
-            </IconButton>
-        )
-    }
-
-    function renderNameCell(row: GridRenderCellParams) {
-        return (
-            <Link
-                style={{ textDecoration: 'none' }}
-                href={`/categories/edit/${row.id}`}
-            >
-                <Typography color={'primary'}>{row.value}</Typography>
-            </Link>
-        )
-    }
-
+  function renderIsActiveCell(row: GridRenderCellParams) {
     return (
-        <Box maxWidth={'lg'} sx={{ mt: 4, mb: 4 }}>
-            <Box display={'flex'} justifyContent="flex-end">
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    component={Link}
-                    href='/categories/create'
-                    style={{ marginBottom: '1rem' }}
-                >
-                    New Category
-                </Button>
-            </Box>
-            <Box sx={{ dispaly: 'flex', heigth: 600 }}>
-                <DataGrid
-                    pageSizeOptions={[2, 20, 50, 100]}
-                    disableRowSelectionOnClick={true}
-                    disableColumnSelector={true}
-                    disableDensitySelector={true}
-                    disableColumnFilter={true}
-                    rows={rows}
-                    columns={columns}
-                    slots={{ toolbar: GridToolbar }}
-                    slotProps={componetsdProps}
-                />
-            </Box>
-        </Box>
-    );
+      <Typography>
+        {row.value ? 'Active' : 'Inactive'}
+      </Typography>
+    )
+  }
+
+  function renderActionsCell(row: GridRenderCellParams) {
+    return (
+      <IconButton
+        color='secondary'
+        onClick={() => console.log(row.value)}
+        aria-label='delete'
+      >
+        <DeleteIcon />
+      </IconButton>
+    )
+  }
+
+  function renderNameCell(row: GridRenderCellParams) {
+    return (
+      <Link
+        style={{ textDecoration: 'none' }}
+        href={`/categories/edit/${row.id}`}
+      >
+        <Typography color={'primary'}>{row.value}</Typography>
+      </Link>
+    )
+  }
+
+  return (
+    <Box maxWidth={'lg'} sx={{ mt: 4, mb: 4 }}>
+      <Box display={'flex'} justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          href='/categories/create'
+          style={{ marginBottom: '1rem' }}
+        >
+          New Category
+        </Button>
+      </Box>
+      <Box sx={{ dispaly: 'flex', heigth: 600 }}>
+        <DataGrid
+          pageSizeOptions={[2, 20, 50, 100]}
+          disableRowSelectionOnClick={true}
+          disableColumnSelector={true}
+          disableDensitySelector={true}
+          disableColumnFilter={true}
+          rows={rows}
+          columns={columns}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={componetsdProps}
+        />
+      </Box>
+    </Box>
+  );
 }
