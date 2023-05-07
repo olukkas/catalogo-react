3413adebbad65c13a62b72cf58a1fc663e0f4027
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-06 23:07:25 -0300

    feat(categories): começãndo layout da página de edição

--- src/features/categories/EditCategory.tsx
+++ src/features/categories/EditCategory.tsx
@@ -1,11 +1,41 @@
-import { Box, Typography } from "@mui/material";
+import {
+    Box,
+    FormControl,
+    Grid,
+    Paper,
+    TextField,
+    Typography
+} from "@mui/material";
 
 export const EditCategory = () => {
     return (
         <Box>
-            <Typography variant={'h3'} component={'h1'}>
-                EditCategory
-            </Typography>
+            <Paper>
+                <Box p={2}>
+                    <Box mb={2}>
+                        <Typography variant="h4">Edit Category</Typography>
+                    </Box>
+                </Box>
+
+                <Box p={2}>
+                    <form>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        name='name'
+                                        label='name'
+                                        value={'0'}
+                                        disabled={false}
+                                        onChange={console.log}
+                                    />
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Box>
+            </Paper>
         </Box>
     );
 }
